f34615436a73c8c6b1fca7803412228a4668271c
Deng <Toby.Deng@student.tdsb.on.ca>
2024-12-09 13:36:57 -0500

    remove button

--- src/views/CartView.jsx
+++ src/views/CartView.jsx
@@ -1,12 +1,15 @@
 import { useStoreContext } from "../context"
 
 const CartView = () => {
-    const { cart } = useStoreContext();
+    const { cart, setCart } = useStoreContext();
     console.log(cart.entrySeq());
     return (
         <div>
             {cart.entrySeq().map(([key, value]) => { return (
-                <p key={key}>{value.title}</p>
+                <>
+                    <p key={key}>{value.title}</p>
+                    <button onClick={() => setCart((prevCart) => prevCart.delete(key))}>Remove</button>
+                </>
             ) })}
         </div>
     )
